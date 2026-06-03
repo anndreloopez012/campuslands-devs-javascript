deb2c39d69213dc22447265e29d05543becdc3cd
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-03 06:44:01 -0600

    feat: solucion ejercicio 05 Jennifer Lopez

--- ejercicios/05-arrays-comida/JenniferLopez.js
+++ ejercicios/05-arrays-comida/JenniferLopez.js
@@ -1,7 +1,23 @@
 // Ejercicio 05: Arrays - comida
-// Completa el codigo siguiendo las instrucciones del README.md.
-
 console.log("Ejercicio 05: Arrays");
 
-// Escribe tu solucion aqui.
+//  ARREGLO 
+const menu = ["Tacos", "Pizza", "Pepián"];
+// push agrega elementos al final de una lista
+menu.push("Hamburguesa"); 
 
+console.log(`\n--- INFORMACIÓN DEL MENÚ ---`);
+console.log(`Total de platillos disponibles: ${menu.length}`); // .length nos da el tamaño
+console.log(`El primer platillo del menú es: ${menu[0]}`);    
+console.log(`El último platillo agregado es: ${menu[3]}`);   
+
+// forEach recorre el arreglo
+console.log(`\n --- MENÚ COMPLETO ---`);
+menu.forEach((comida, i) => console.log(`${i + 1}. ${comida}`));
+
+// Revisamos si un platillo existe en nuestro menú usando .includes() que devuelve true/false
+console.log(`\n--- ESPECIALES---`);
+const buscarPlatillo = "Pepián";
+const existePlatillo = menu.includes(buscarPlatillo);
+
+console.log(`¿Tenemos el platillo "${buscarPlatillo}" en el menú?: ${existePlatillo}`);
