b7347844a14bcd27795e54668e4416f54f922043
lemical07 <icaleladio@gmail.com>
2026-06-03 10:26:28 -0600

    feat: mostrar tipos de datos con ejemplo de moto
    Variables de string, number, boolean, null, undefined y array . Uso de typeof para identificar cada tipo.

--- ejercicios/02-tipos-datos-motos/ejercicio.js
+++ ejercicios/02-tipos-datos-motos/ejercicio.js
@@ -5,3 +5,33 @@ console.log("Ejercicio 02: Tipos de datos");
 
 // Escribe tu solucion aqui.
 
+const marca = "Yamaha";
+const modelo = "MT-07";
+const cilindrada = 689;
+const esDeportiva = false;
+const precio = 8500;
+
+let kilometraje = null;
+let propietario = undefined;
+
+const caracteristicas = ["ABS", "Control de tracción", "Pantalla TFT", "Escape Akrapovic"];
+
+console.log("=== INFORMACIÓN DE LA MOTO ===");
+console.log("Marca:", marca, "- Tipo:", typeof marca);
+console.log("Modelo:", modelo, "- Tipo:", typeof modelo);
+console.log("Cilindrada:", cilindrada, "cc - Tipo:", typeof cilindrada);
+console.log("Es deportiva:", esDeportiva, "- Tipo:", typeof esDeportiva);
+console.log("Precio: $", precio, "- Tipo:", typeof precio);
+console.log("Kilometraje:", kilometraje, "- Tipo:", typeof kilometraje);
+console.log("Propietario:", propietario, "- Tipo:", typeof propietario);
+
+console.log("\n=== CARACTERÍSTICAS ===");
+console.log(caracteristicas);
+
+// Reto extra
+const velocidadMaxima = 210;
+const puedeSuperar200 = velocidadMaxima > 200;
+
+console.log(`\n=== DATO EXTRA ==`);
+console.log("Velocidad máxima",velocidadMaxima, "km/h");
+console.log("¿Puede superar los 200 km/h", puedeSuperar200);
