15a59cb5ae05ed3928bc2fe06ba086b2a8c29db5
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 11:07:55 -0600

    :sparkles: Feat(ejercicio-07): Ejericio-07 de bucles for/while terminado

--- ejercicios/07-bucles-ropa/ejercicio.js
+++ ejercicios/07-bucles-ropa/ejercicio.js
@@ -5,3 +5,18 @@ console.log("Ejercicio 07: Bucles for/while");
 
 // Escribe tu solucion aqui.
 
+console.log("========== Bucles for/while - ropa ==========");
+
+console.log("\nUsando un bucle for:");
+const ropa = ["camisa", "pantalon", "zapatos", "sombrero", "calcetines"];
+
+for(let i = 0; i < ropa.length; i++) {
+    console.log(ropa[i]);
+}
+
+console.log("\nUsando un bucle while:");
+
+while(ropa.length > 0) {
+    const prenda = ropa.pop();
+    console.log(prenda);
+}
