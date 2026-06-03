493a46cec7855270072cf043347fa9974fa4f53d
MariaJoseMontepequeZet <montepequemajo@gmail.com>
2026-06-03 10:40:54 -0600

    resolver ejercicio 01 de variables y constantes: Declaro constantes para los datos fijos de la partida (jugador, vida y municion maxima) y variables para los que cambian (arma, vida, municion, estado). Simulo combate con reasignaciones y agrego validaciones de vida y recarga como reto extra.

---
+++ ejercicios/01-variables-videojuegos-shooter/maria-montepeque.js
@@ -0,0 +1,14 @@
+// Ejercicio 01: Variables y constantes - videojuegos shooter
+console.log("Ejercicio 01: Variables y constantes");
+
+const jugador = "Majo_Sniper", vidaMax = 100, municionMax = 30;
+let arma = "Rifle", vida = vidaMax, municion = municionMax, estado = "en curso";
+
+console.log(`${jugador} | ${arma} | Vida ${vida}/${vidaMax} | Municion ${municion}/${municionMax} | ${estado}`);
+
+vida -= 35; municion -= 12; arma = "Escopeta"; // combate
+
+if (vida <= 0) { estado = "perdida"; vida = 0; }
+if (municion < 10) municion = municionMax; // recarga
+
+console.log(`${jugador} | ${arma} | Vida ${vida}/${vidaMax} | Municion ${municion}/${municionMax} | ${estado}`);
