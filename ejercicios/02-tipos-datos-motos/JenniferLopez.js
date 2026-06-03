bc2db4daee95a56f80ad55e106f8ae5eb00f9427
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-03 00:24:22 -0600

    feat: solucion ejercicio 02 Jennifer Lopez

--- ejercicios/02-tipos-datos-motos/JenniferLopez.js
+++ ejercicios/02-tipos-datos-motos/JenniferLopez.js
@@ -1,7 +1,19 @@
 // Ejercicio 02: Tipos de datos - motos
-// Completa el codigo siguiendo las instrucciones del README.md.
-
 console.log("Ejercicio 02: Tipos de datos");
 
-// Escribe tu solucion aqui.
+//creacion de datos
+const marca = "Kawasaki Ninja ZX-10R";           
+const cilindraje = 998;                   
+const tieneFrenosAbs = true;              
+let propietarioAnterior = null;           
+let proximoMantenimiento;                 
+const Disponibles = ["Rojo", "Negro", "Gris"]; 
 
+// mostrara valores y tipos de datos
+console.log("\n--- Datos DE LA MOTO ---\n");
+console.log(`Moto: ${marca} | Tipo de dato: ${typeof marca}`);
+console.log(`Cilindraje: ${cilindraje}cc | Tipo de dato: ${typeof cilindraje}`);
+console.log(`¿Tiene ABS?: ${tieneFrenosAbs} | Tipo de dato: ${typeof tieneFrenosAbs}`);
+console.log(`Propietario anterior: ${propietarioAnterior} | Tipo de dato: ${typeof propietarioAnterior}`);
+console.log(`Próximo mantenimiento: ${proximoMantenimiento} | Tipo de dato: ${typeof proximoMantenimiento}`);
+console.log(`Colores de fábrica: ${Disponibles} | Tipo de dato: ${typeof Disponibles}`);
