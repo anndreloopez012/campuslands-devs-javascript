df3f1e65e8beb8c8a5c265d2af542a617afd58ba
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 13:56:31 -0600

    :sparkles: Feat(ejercicio-09): Ejercicio-09 funciones-pingpong terminada

--- ejercicios/09-funciones-pingpong/ejercicio.js
+++ ejercicios/09-funciones-pingpong/ejercicio.js
@@ -5,3 +5,39 @@ console.log("Ejercicio 09: Funciones");
 
 // Escribe tu solucion aqui.
 
+console.log("========== Funciones - pingpong ==========")
+
+let puntosJ1 = 10
+let puntosJ2 = 9
+
+function puntos(jugador){
+
+    if (jugador == "J1"){
+        puntosJ1 +=1
+    }else if (jugador == "J2"){
+        puntosJ2 +=1
+    }
+
+    return puntosJ1, puntosJ2
+
+}
+
+function ganador(n1, n2){
+
+    if (n1 <= 11 && n1 > n2){
+        mensaje = "El jugador 1 es el ganador"
+    }else if(n2 <= 11 && n2 > n1){
+        mensaje = "El jugador 2 es el ganador"
+    }else{
+        mensaje = "Empates"
+    }
+
+    return mensaje
+}
+
+function mensajeFinal(mensaje){
+    console.log(mensaje)
+}
+
+
+mensajeFinal(ganador(puntosJ1, puntosJ2))
