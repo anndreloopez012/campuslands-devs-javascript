93bdf0364236ae119bd6103be0967403aaf97499
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-03 01:00:31 -0600

    feat: solucion ejercicio 04 Jennifer Lopez

--- ejercicios/04-condicionales-videojuegos-futbol/JenniferLopez.js
+++ ejercicios/04-condicionales-videojuegos-futbol/JenniferLopez.js
@@ -1,7 +1,15 @@
 // Ejercicio 04: Condicionales if/else - videojuegos de futbol
-// Completa el codigo siguiendo las instrucciones del README.md.
-
 console.log("Ejercicio 04: Condicionales if/else");
+const golEquipo1=6;
+const golEquipo2=3;
+console.log(`\n Fin del partido | Marcador: Barcelona ${golEquipo1} - ${golEquipo2} Real Madrid \n`);
 
-// Escribe tu solucion aqui.
-
+if (golEquipo1 > golEquipo2) {
+    console.log("¡VICTORIA PARA EL BARCELONA! Suma 6 puntos en al torneo.");
+} 
+else if (golEquipo1 === golEquipo2) {
+    console.log(" ¡EMPATE! Ambos equipos se llevan 1 punto.");
+} 
+else {
+    console.log(" ¡DERROTA para el Barcelona! No suma puntos en esta jornada.");
+}
