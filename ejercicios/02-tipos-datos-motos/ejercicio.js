82565ca3d74e1eff642ddde9f2b07484343e9730
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 06:35:01 -0600

    :sparkles: Feat(ejercicio-02): Ejercicio-02-tipos-datos-motos terminado

--- ejercicios/02-tipos-datos-motos/ejercicio.js
+++ ejercicios/02-tipos-datos-motos/ejercicio.js
@@ -5,3 +5,14 @@ console.log("Ejercicio 02: Tipos de datos");
 
 // Escribe tu solucion aqui.
 
+
+console.log("========== MOTOS =========")
+
+let nombreMotos = "Suzuki"
+let cantidadMotos = 50
+let buenEstado = true
+let numMalEstado = null
+const modelos = ["GN125", "AX4", "Avenis 125", "Gixxer 155", "Gixxer 250", "serie V-storm"]
+let consideraciones
+
+console.log(`Motos: ${nombreMotos}\nCantidad de motos: ${cantidadMotos}\nEstados de todas las motos: ${buenEstado}\nMotos en mal estado: ${numMalEstado}\nModelos: ${modelos}\nConsideraciones extras: ${consideraciones}`)
