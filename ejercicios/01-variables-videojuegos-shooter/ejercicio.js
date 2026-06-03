6994f1072ea445d268c68ef989a99b4769993874
danielAVAR <danielaguilar5502@gmail.com>
2026-06-03 13:08:19 -0600

    feat: Resolvi ejercicio 1

--- ejercicios/01-variables-videojuegos-shooter/ejercicio.js
+++ ejercicios/01-variables-videojuegos-shooter/ejercicio.js
@@ -5,3 +5,18 @@ console.log("Ejercicio 01: Variables y constantes");
 
 // Escribe tu solucion aqui.
 
+const Jugador = "juador1"; 
+
+let arma = "basuca";
+
+let municion = 10;
+
+let vida = 100;
+
+let estado_de_partida = "en curso";
+
+console.log("Jugador: " + Jugador);
+console.log("Arma: " + arma);
+console.log("Municion: " + municion);
+console.log("Vida: " + vida);
+console.log("Estado de partida: " + estado_de_partida);
