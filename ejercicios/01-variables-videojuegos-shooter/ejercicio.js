e011f6a7d6f1995deb5e272ac626108741719a54
albertojavieralvarez <bienesdeluxe@gmail.com>
2026-06-03 06:15:01 -0600

    :sparkles: Feat(Ejercicio-01): Ejercicio-01 de js terminado

--- ejercicios/01-variables-videojuegos-shooter/ejercicio.js
+++ ejercicios/01-variables-videojuegos-shooter/ejercicio.js
@@ -5,3 +5,16 @@ console.log("Ejercicio 01: Variables y constantes");
 
 // Escribe tu solucion aqui.
 
+const jugador = 'Alondra'
+let vida = '150 HP'
+
+const arma = 'Locus'
+let municion = '30'
+
+const estadoPartida = ['Emparejamiento', 'Pre-partida', 'Partida en curso', 'Post-partida']
+
+console.log(jugador)
+console.log(vida)
+console.log(arma)
+console.log(municion)
+console.log(estadoPartida)
