02b908b8b880c3b0d1196274fbc895910ca83661
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 10:53:18 -0600

    :sparkles: Feat(ejercicio-05): Ejercicio-05 arrays-comida terminado

--- ejercicios/05-arrays-comida/ejercicio.js
+++ ejercicios/05-arrays-comida/ejercicio.js
@@ -5,3 +5,16 @@ console.log("Ejercicio 05: Arrays");
 
 // Escribe tu solucion aqui.
 
+console.log("========== Arrays - comida ==========");
+
+const comida = ["Pizza", "Hamburguesa", "Tacos", "Sushi", "Ensalada"];
+
+console.log(comida)
+
+comida.push("Pasta");
+
+comida.forEach((platillo, index) => {
+  console.log(`${index + 1}. ${platillo}`);
+});
+
+console.log("Platillo favorito en poscion 3:", comida[2]);
