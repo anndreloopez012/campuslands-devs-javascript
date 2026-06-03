71b5f9275c2fe6f43ac1f0e3f0b1a6dadb8cdecf
ArrivillagaDAS <sfaaldana101@gmail.com>
2026-06-03 16:14:15 -0600

    feat: solve exercise 06

--- ejercicios/06-objetos-viajes/ejercicio.js
+++ ejercicios/06-objetos-viajes/ejercicio.js
@@ -5,3 +5,30 @@ console.log("Ejercicio 06: Objetos");
 
 // Escribe tu solucion aqui.
 
+// Ejercicio 06: Objeto de viajes
+
+let viaje = {
+    destino: "Tokio",
+    dias: 7,
+    presupuesto: 2000,
+    
+    // metodo de resumen
+    resumen: function() {
+        return `Viaje a ${this.destino} por ${this.dias} días con un presupuesto de $${this.presupuesto}.`;
+    }
+};
+
+// Consultar propiedades
+console.log(viaje.resumen());
+
+// Reto extra
+// validacion de presupuesto
+viaje.esAsequible = function(costoEstimado) {
+    if (this.presupuesto >= costoEstimado) {
+        return "¡El presupuesto es suficiente!";
+    } else {
+        return "Necesitas ahorrar un poco más.";
+    }
+};
+
+console.log(viaje.esAsequible(1500));
