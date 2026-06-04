891cb555e8378a1e1c5cca80e091893e0b942ce1
danielAVAR <danielaguilar5502@gmail.com>
2026-06-03 22:00:52 -0600

    feat: resolvi ejercicio 4

--- ejercicios/04-condicionales-videojuegos-futbol/ejercicio.js
+++ ejercicios/04-condicionales-videojuegos-futbol/ejercicio.js
@@ -5,3 +5,18 @@ console.log("Ejercicio 04: Condicionales if/else");
 
 // Escribe tu solucion aqui.
 
+// input de goles anotados por ambos equipos
+
+let goles_equipo1 = parseInt(prompt("goles equipo 1:"));
+let goles_equipo2 = parseInt(prompt("goles equipo 2:"));
+
+//condicionales para determinar el resultado del partido
+
+if (goles_equipo1 > goles_equipo2) {
+    console.log("Equipo 1 gana");
+} else if (goles_equipo2 > goles_equipo1) {
+    console.log("Equipo 2 gana");
+} else {
+    console.log("Empate");
+}
+
