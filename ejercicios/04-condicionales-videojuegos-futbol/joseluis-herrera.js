9435dda7cb552b3b4440d8353d6428aeac000e42
Jose-langL <totherrerajoseluis@gmail.com>
2026-06-03 20:11:44 -0600

    feat(ejercicio04) Resolver ejercicio de condicionales

---
+++ ejercicios/04-condicionales-videojuegos-futbol/joseluis-herrera.js
@@ -0,0 +1,14 @@
+//Dato de goles
+let equipoA = 3
+let equipoB = 2
+
+//Validacion
+if(equipoA > equipoB){
+    console.log("El equipo A es el ganador")
+    console.log("El equipo B es el perdedor")
+}else if(equipoB > equipoA){
+    console.log("El equipo B es el ganador")
+    console.log("El equipo A es el perdedor")
+}else{
+    console.log("Hay un empate entre los equipos")
+}
