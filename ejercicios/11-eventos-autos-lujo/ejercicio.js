29b00fe3ce7bc23f14c303b95ebe8189bb6c6dab
ArrivillagaDAS <sfaaldana101@gmail.com>
2026-06-03 21:22:11 -0600

    feat: solve exercise 11

--- ejercicios/11-eventos-autos-lujo/ejercicio.js
+++ ejercicios/11-eventos-autos-lujo/ejercicio.js
@@ -5,3 +5,29 @@ console.log("Ejercicio 11: Eventos");
 
 // Escribe tu solucion aqui.
 
+// Seleccionamos elementos
+const titulo = document.querySelector("#titulo");
+const selector = document.querySelector("#selectorModelo");
+const btnActualizar = document.querySelector("#btnActualizar");
+
+// Escuchar el evento
+btnActualizar.addEventListener("click", () => {
+    const modeloElegido = selector.value;
+    
+    titulo.textContent = `Auto seleccionado: ${modeloElegido}`;
+    console.log("El usuario eligió:", modeloElegido);
+});
+
+// Reto Extra
+const configuraciones = {
+    "Ferrari": "#ff0000",
+    "Lamborghini": "#f1c40f",
+    "Porsche": "#2e2e2e"
+};
+
+// Evento
+btnActualizar.addEventListener("click", () => {
+    const modeloElegido = selector.value;
+    const colorSeleccionado = configuraciones[modeloElegido] || "#19a974";
+    visualizador.style.backgroundColor = colorSeleccionado;
+});
