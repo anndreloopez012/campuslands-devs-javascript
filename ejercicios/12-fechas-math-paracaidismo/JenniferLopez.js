6ea1c4ca7c4e54c6c39cbc8702509582cd7df3a3
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-04 06:25:55 -0600

    feat: solucion del ejercicio 12 fechas math paracaidismo Jennifer Lopez

--- ejercicios/12-fechas-math-paracaidismo/JenniferLopez.js
+++ ejercicios/12-fechas-math-paracaidismo/JenniferLopez.js
@@ -1,7 +1,27 @@
 // Ejercicio 12: Fechas y Math - paracaidismo
-// Completa el codigo siguiendo las instrucciones del README.md.
-
 console.log("Ejercicio 12: Fechas y Math");
 
-// Escribe tu solucion aqui.
+const fechaActual = new Date();
+
+const fechaFormateada = fechaActual.toLocaleDateString();
+const horaFormateada = fechaActual.toLocaleTimeString();
+
+const alturaSalto = Math.round(Math.random() * (4000 - 1000) + 1000);
+
+const tiempoCaidaLibre = Math.round(Math.random() * (60 - 30) + 30);
+
+let reporteSeguridad = " Apertura normal del paracaídas principal.";
+
+if (alturaSalto <= 1500) {
+    reporteSeguridad = " ¡ADVERTENCIA! Altura crítica. Se activó el sistema de apertura automática de emergencia (CYPRES).";
+}
+
+console.log("\n --- REPORTE OFICIAL DE SALTO TÁNDEM ---");
+console.log(` Fecha del salto : ${fechaFormateada}`);
+console.log(`  Hora de salida  : ${horaFormateada}`);
+console.log(`  Altura del salto: ${alturaSalto} metros`);
+console.log(` Caída libre     : ${tiempoCaidaLibre} segundos`);
+
+console.log("\n--- BITÁCORA DE SEGURIDAD (Reto Extra) ---");
+console.log(reporteSeguridad);
 
