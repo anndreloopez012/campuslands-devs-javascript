ebe4875884db2945fcadb3158221503ff48a996c
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 20:39:40 -0600

    :sparkles: Feat(ejercicio-11): Ejercicio-11 Eventos-autos de lujo terminados

--- ejercicios/11-eventos-autos-lujo/ejercicio.js
+++ ejercicios/11-eventos-autos-lujo/ejercicio.js
@@ -5,3 +5,17 @@ console.log("Ejercicio 11: Eventos");
 
 // Escribe tu solucion aqui.
 
+let main = document.querySelector(".panel")
+let titulo = document.querySelector("#titulo")
+let descripcion = document.querySelector("#descripcion")
+let boton = document.querySelector("#accion")
+
+
+boton.addEventListener("click", function() {
+    main.style.backgroundColor = "black"
+    titulo.style.color = "white"
+    descripcion.style.color = "white"
+    boton.style.backgroundColor = "green" 
+    titulo.textContent = "Lamborghini Aventador"
+    descripcion.textContent = "El Lamborghini Aventador es un superdeportivo de lujo fabricado por la marca italiana Lamborghini. Con su diseño agresivo y su potente motor V12, el Aventador ofrece un rendimiento excepcional y una experiencia de conducción emocionante. Es conocido por su velocidad, manejo preciso y exclusividad, convirtiéndolo en uno de los autos más deseados del mundo."
+})
