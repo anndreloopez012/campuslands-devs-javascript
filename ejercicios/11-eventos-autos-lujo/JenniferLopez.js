6bab2fcf5fa15574d92bbbb3fda4b3adc919d96c
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-04 05:03:36 -0600

    Refactor: correccion de lineas de codigo

--- ejercicios/11-eventos-autos-lujo/JenniferLopez.js
+++ ejercicios/11-eventos-autos-lujo/JenniferLopez.js
@@ -6,37 +6,34 @@ const descripcion = document.querySelector("#descripcion");
 const boton = document.querySelector("#accion");
 const panel = document.querySelector(".panel");
 
-titulo.textContent = " Ficha Técnica: Deportivo";
-descripcion.textContent = "Modelo: Ferrari | Velocidad: 0 km/h";
-boton.textContent = "Cambiar Color";
+let modeloActual = "Ferrari";
+let velocidadActual = 0;
 
-const controlVelocidad = document.createElement("input");
-controlVelocidad.type = "range";
-controlVelocidad.min = "0";
-controlVelocidad.max = "350";
-controlVelocidad.value = "0";
-controlVelocidad.style.display = "block";
-controlVelocidad.style.margin = "15px 0";
+titulo.textContent = "🏎️ Ficha: Superdeportivo";
+descripcion.textContent = `Modelo: ${modeloActual} | Velocidad: ${velocidadActual} km/h`;
+boton.textContent = "Cambiar Auto y Color";
 
-panel.insertBefore(controlVelocidad, boton);
+const barraVelocidad = document.createElement("input");
+barraVelocidad.type = "range";
+barraVelocidad.min = "0";
+barraVelocidad.max = "350";
+barraVelocidad.value = "0";
+panel.insertBefore(barraVelocidad, boton);
 
-controlVelocidad.addEventListener("input", (evento) => {
 
-    const velocidadActual = evento.target.value;
-    descripcion.textContent = `Modelo: Ferrari LaFerrari | Velocidad: ${velocidadActual} km/h`;
+barraVelocidad.addEventListener("input", (evento) => {
+    velocidadActual = evento.target.value; 
+    descripcion.textContent = `Modelo: ${modeloActual} | Velocidad: ${velocidadActual} km/h`;
 });
 
 boton.addEventListener("click", () => {
-
-    panel.style.background = panel.style.background === "rgb(255, 204, 204)" ? "#ffffff" : "#ffcccc"; 
-});
-
-
-const alertaPista = document.createElement("p");
-alertaPista.style.fontWeight = "bold";
-panel.appendChild(alertaPista);
-
-controlVelocidad.addEventListener("input", (evento) => {
-    alertaPista.textContent = evento.target.value > 300 ? " ¡ADVERTENCIA: Modo Pista Activo!" : "";
-    alertaPista.style.color = "red";
+    if (modeloActual === "Ferrari") {
+        modeloActual = "Lamborghini";
+        panel.style.background = "#fff2cc"; 
+    } else {
+        modeloActual = "Ferrari";
+        panel.style.background = "#ffcccc"; 
+    }
+    
+    descripcion.textContent = `Modelo: ${modeloActual} | Velocidad: ${velocidadActual} km/h`;
 });
