b96ae6f2ca8cdc01e0eeab423c69c86a993f674e
lemical07 <icaleladio@gmail.com>
2026-06-03 22:25:23 -0600

    feat: simular bitacora de paracaidismo usando objetos Date, calculos con Math y validacion de altitud

--- ejercicios/12-fechas-math-paracaidismo/ejercicio.js
+++ ejercicios/12-fechas-math-paracaidismo/ejercicio.js
@@ -5,3 +5,35 @@ console.log("Ejercicio 12: Fechas y Math");
 
 // Escribe tu solucion aqui.
 
+const fechaActual = new Date()
+
+const dia = fechaActual.getDate()
+const mes = fechaActual.getMonth() + 1;
+const año = fechaActual.getFullYear()
+const hora = fechaActual.getHours()
+const minutos = fechaActual.getMinutes()
+
+const alturaMinima = 2000;
+const alturaMaxima = 5000;
+const alturaAleatoria = Math.random() * (alturaMaxima - alturaMinima) + alturaMinima
+const alturaFinal = Math.round(alturaAleatoria)
+
+const tiempoCaidaLibre = Math.round(Math.random() * (60 - 30) + 30)
+
+console.log("=== BITÁCORA DE PARACAIDISMO ===")
+console.log("Fecha del salto:", dia, "/", mes, "/", año)
+console.log("Hora de despegue:", hora, ":", minutos)
+
+console.log("\n=== DATOS DEL TELEMETRÍA ===");
+console.log("Altura de lanzamiento:", alturaFinal, "metros sobre el nivel del mar.")
+console.log("Tiempo estimado de caída libre:", tiempoCaidaLibre, "segundos.")
+
+// extra: validación de seguridad aeromédica
+console.log("\n=== PROTOCOLO DE SEGURIDAD ===");
+const limiteOxigeno = 4000;
+
+if (alturaFinal > limiteOxigeno) {
+  console.log("Validación: Salto de alta altitud. Se activó el sistema de oxígeno suplementario en la cabina.");
+} else {
+  console.log("Validación: Altitud estándar de salto. Respiración autónoma permitida.");
+}
