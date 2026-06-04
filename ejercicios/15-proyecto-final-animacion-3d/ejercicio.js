7edd5463f916edd187669bf146a29470bfe44eec
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-04 14:25:41 -0600

    :sparkles: Feat(ejercicio-15): Ejercicio-15 animacion terminado

--- ejercicios/15-proyecto-final-animacion-3d/ejercicio.js
+++ ejercicios/15-proyecto-final-animacion-3d/ejercicio.js
@@ -5,3 +5,12 @@ console.log("Ejercicio 15: Proyecto integrador basico");
 
 // Escribe tu solucion aqui.
 
+let tower = document.querySelector(".tower")
+let cube = document.querySelector(".cube")
+let toggle = document.querySelector("#toggle")
+
+toggle.addEventListener("click", () => {
+    tower.classList.toggle("rotatingTower")
+    cube.classList.toggle("rotatingCube")
+})
+
