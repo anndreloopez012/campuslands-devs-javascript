455217dd412391e163734c6c64fcde62ce099b5b
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 21:54:41 -0600

    :bug: Fix(ejercicio-13): Correccion de los objetos

--- ejercicios/13-json-libros-peliculas/ejercicio.js
+++ ejercicios/13-json-libros-peliculas/ejercicio.js
@@ -7,8 +7,19 @@ console.log("Ejercicio 13: JSON");
 
 console.log("========== JSON - Libros y Peliculas ==========");
 
-const libros = ["La vuelta al mundo en 80 dias", "El principe y el mendigo", "El conde de montecristo", "Dueño del mundo", "El hombre invisible "];
-const peliculas = ["El señor de los anillos", "El hobbit", "El rey leon", "El libro de la selva", "El diario de una pasion"];
+const libros = [
+    {genero: "ciencia ficcion", titulo: "La vuelta al mundo en 80 dias"}, 
+    {genero: "ciencia ficcion", titulo: "El principe y el mendigo"}, 
+    {genero: "ciencia ficcion", titulo: "El conde de montecristo"},
+    {genero: "ciencia ficcion", titulo: "Dueño del mundo"},
+    {genero: "ciencia ficcion", titulo: "El hombre invisible"}
+];
+const peliculas = [
+    {titulo: "El señor de los anillos"}, {titulo: "El hobbit"},
+    {titulo: "El rey leon"}, 
+    {titulo: "El libro de la selva"}, 
+    {titulo: "El diario de una pasion"}
+];
 
 const librosJSON = JSON.stringify(libros);
 const peliculasJSON = JSON.stringify(peliculas);
