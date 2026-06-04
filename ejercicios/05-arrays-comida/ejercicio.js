8cf120cd21ba723c60a4d9dd1d3d86e2d053e517
steff-360 <steff.18random@gmail.com>
2026-06-04 06:48:40 -0600

    Resolucion ejercicio 05

--- ejercicios/05-arrays-comida/ejercicio.js
+++ ejercicios/05-arrays-comida/ejercicio.js
@@ -5,3 +5,35 @@ console.log("Ejercicio 05: Arrays");
 
 // Escribe tu solucion aqui.
 
+console.log("Ejercicio 05: Arrays");
+
+// Crear arreglo de comidas favoritas
+let menu = ["Pizza", "Hamburguesa", "Tacos"];
+
+// Agregar elementos
+menu.push("Pasta");
+menu.push("Sushi");
+
+// Mostrar arreglo completo
+console.log("Menú:", menu);
+
+// Consultar posiciones
+console.log("Primera comida:", menu[0]);
+console.log("Última comida:", menu[menu.length - 1]);
+
+// Mostrar cantidad de elementos
+console.log("Cantidad de comidas:", menu.length);
+
+// Recorrer el arreglo
+menu.forEach((comida, indice) => {
+    console.log(`${indice + 1}. ${comida}`);
+});
+
+// Reto extra
+let comidaFavorita = "Pizza";
+
+if (menu.includes(comidaFavorita)) {
+    console.log(comidaFavorita + " está en el menú.");
+} else {
+    console.log(comidaFavorita + " no está en el menú.");
+}
