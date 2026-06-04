34533a2abffd5649ca6f1849c507fdd13d13f8cc
lemical07 <icaleladio@gmail.com>
2026-06-03 21:55:31 -0600

    feat: crear funciones modulares para marcador de pingpong y calculo de efectividad de saque

--- ejercicios/09-funciones-pingpong/ejercicio.js
+++ ejercicios/09-funciones-pingpong/ejercicio.js
@@ -5,3 +5,44 @@ console.log("Ejercicio 09: Funciones");
 
 // Escribe tu solucion aqui.
 
+// extra:calcular la efectividad del saque
+function calcularEfectSaque(saquesTotales, saquesAses) {
+  if (saquesTotales === 0) {
+    return 0;
+  }
+  return (saquesAses / saquesTotales) * 100;
+}
+
+function determinarGanador(puntosJose, puntosDhoman) {
+  const puntosGanar = 5;
+
+  if (puntosJose >= puntosGanar && puntosJose > puntosDhoman) {
+    return "Ganador del partido: José";
+  } 
+  else if (puntosDhoman >= puntosGanar && puntosDhoman > puntosJose) {
+    return "Ganador del partido: Dhoman";
+  } 
+  else {
+    return "El partido sigue en juego...";
+  }
+}
+
+function generarReportePartido(nombreA, puntosA, nombreB, puntosB) {
+  console.log("=== TABLERO DE PING-PONG ===");
+  console.log(nombreA, ":", puntosA, "puntos");
+  console.log(nombreB, ":", puntosB, "puntos");
+  
+  const resultado = determinarGanador(puntosA, puntosB);
+  console.log("Estado:", resultado);
+}
+
+// --- EJECUCIÓN DEL SCRIPT ---
+
+// exta: simulación de estadisticas
+const efectividadJose = calcularEfectSaque(10, 4); //saques y ases
+console.log("=== ESTADÍSTICAS DE RENDIMIENTO ===");
+console.log("Efectividad de saque de José:", efectividadJose, "%");
+
+console.log(""); 
+
+generarReportePartido("José", 5, "Dhoman", 3);
