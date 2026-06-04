44d517cc6703c329051d0b567117536a340b7b61
lemical07 <icaleladio@gmail.com>
2026-06-03 21:12:42 -0600

    feat: implementar control de inventario de ropa usando bucles for, while y alerta de stock critico

--- ejercicios/07-bucles-ropa/ejercicio.js
+++ ejercicios/07-bucles-ropa/ejercicio.js
@@ -5,3 +5,28 @@ console.log("Ejercicio 07: Bucles for/while");
 
 // Escribe tu solucion aqui.
 
+const prendasCatalogo = ["Chaqueta", "Saco", "Sudadera"];
+const ventasPorCiclo = 2;
+const limiteCritico = 4;
+let stockInventario = 10;
+
+console.log("=== CATÁLOGO DE PRENDAS ===");
+for (let i = 0; i < prendasCatalogo.length; i++) {
+  const numeroItem = i + 1;
+  console.log("Artículo", numeroItem, ":", prendasCatalogo[i]);
+}
+
+console.log("\n=== INICIO DE SIMULACIÓN DE VENTAS ===");
+console.log("Stock inicial de la tienda:", stockInventario, "unidades.");
+
+while (stockInventario > 0) {
+  stockInventario = stockInventario - ventasPorCiclo;
+  console.log("Se vendieron", ventasPorCiclo, "prendas. Stock restante:", stockInventario);
+
+  // extra: validación de stovk critico
+  if (stockInventario === limiteCritico) {
+    console.log("ALERTAS DEL SISTEMA: ¡Stock Crítico alcanzado! Solo quedan", stockInventario, "unidades.");
+  }
+}
+
+console.log("\nSimulación finalizada: Inventario completamente agotado.");
