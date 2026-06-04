1b65ba0592913bc4a1e8e0140aeb30513aaffb24
danielAVAR <danielaguilar5502@gmail.com>
2026-06-03 22:31:22 -0600

    feat: resolvi ejercicio 5

--- ejercicios/05-arrays-comida/ejercicio.js
+++ ejercicios/05-arrays-comida/ejercicio.js
@@ -5,3 +5,21 @@ console.log("Ejercicio 05: Arrays");
 
 // Escribe tu solucion aqui.
 
+const comidas_fav = ["pizza", "hamburguesa", "sushi", "tacos", "atun"];
+
+console.log(comidas_fav);
+// imprime la longitud del array 
+console.log(comidas_fav.length);
+
+// agrega un nuevo elemento al final del array
+comidas_fav.push("pasta");
+console.log(comidas_fav);
+console.log(comidas_fav.length);
+
+// accede al tercer elemento del array
+console.log(comidas_fav[2]);
+
+// recorre el array e imprima cada comida favorita en una nueva linea
+comidas_fav.forEach((comidas) => {
+    console.log(comidas);
+});
