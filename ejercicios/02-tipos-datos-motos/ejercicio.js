44c999f9ae20e02853335f52cd9531046f7fe4f9
steff-360 <steff.18random@gmail.com>
2026-06-04 06:34:23 -0600

    Resoluciones ejercicio 02

--- ejercicios/02-tipos-datos-motos/ejercicio.js
+++ ejercicios/02-tipos-datos-motos/ejercicio.js
@@ -5,3 +5,42 @@ console.log("Ejercicio 02: Tipos de datos");
 
 // Escribe tu solucion aqui.
 
+console.log("Ejercicio 02: Tipos de datos");
+
+// String
+let marca = "Yamaha";
+
+// Number
+let cilindrada = 150;
+
+// Boolean
+let encendida = true;
+
+// Null
+let propietario = null;
+
+// Undefined
+let color;
+
+// Array
+let accesorios = ["Casco", "Maletero", "Luces LED"];
+
+// Mostrar valores
+console.log("Marca:", marca);
+console.log("Cilindrada:", cilindrada);
+console.log("Encendida:", encendida);
+console.log("Propietario:", propietario);
+console.log("Color:", color);
+console.log("Accesorios:", accesorios);
+
+// Mostrar tipos de datos
+console.log(typeof marca);
+console.log(typeof cilindrada);
+console.log(typeof encendida);
+console.log(typeof propietario);
+console.log(typeof color);
+console.log(typeof accesorios);
+
+// Reto extra
+let velocidadMaxima = 120;
+console.log("Velocidad máxima:", velocidadMaxima, "km/h");
