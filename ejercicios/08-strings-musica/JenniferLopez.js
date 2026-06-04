1092055c22c3dc49146f538a47d50bd2c03f1264
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-03 22:41:07 -0600

    Feat: se creo solucion para el ejercicio 08 strigs musica Jennifer Lopez

--- ejercicios/08-strings-musica/JenniferLopez.js
+++ ejercicios/08-strings-musica/JenniferLopez.js
@@ -1,7 +1,24 @@
 // Ejercicio 08: Strings y metodos - musica
-// Completa el codigo siguiendo las instrucciones del README.md.
-
 console.log("Ejercicio 08: Strings y metodos");
 
-// Escribe tu solucion aqui.
+const cancionSucia = "    Plan A - Paulo Londra    ";
+console.log("\n --- PROCESAMIENTO DE AUDIO ---");
+
+const cancionLimpia = cancionSucia.trim();
+console.log(`Texto corregido con .trim(): "${cancionLimpia}"`);
+
+console.log(`Título en MAYÚSCULAS: ${cancionLimpia.toUpperCase()}`);
+
+console.log(`Formato de radio: ${cancionLimpia.replace("-", "by")}`);
+
+console.log(`Nombre de la canción sola: ${cancionLimpia.slice(0, 6)}`);
+
+
+console.log("\n --- MONITOREO DE TRACKS ---");
+
+const trackCompleto = "Rusherking - Los Del Espacio Ft. LIT killah, Tiago";
+const esColaboracion = trackCompleto.includes("Ft.");
+
+console.log(`Track analizado: ${trackCompleto}`);
+console.log(`¿Este tema incluye una colaboración?: ${esColaboracion}`);
 
