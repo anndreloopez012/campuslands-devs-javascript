dbe43f52c3a18eca2d1b5ed9029e665332fd27da
ArrivillagaDAS <sfaaldana101@gmail.com>
2026-06-03 21:41:50 -0600

    feat: solve exercise 12

--- ejercicios/12-fechas-math-paracaidismo/ejercicio.js
+++ ejercicios/12-fechas-math-paracaidismo/ejercicio.js
@@ -5,3 +5,32 @@ console.log("Ejercicio 12: Fechas y Math");
 
 // Escribe tu solucion aqui.
 
+// fecha de salto
+const fechaSalto = new Date();
+const fechaFormateada = fechaSalto.toLocaleDateString("es-ES", {
+    day: '2-digit', month: 'long', year: 'numeric'
+});
+
+// datos aleatorios usando math
+// altura
+const altura = Math.round(Math.random() * (4500 - 3000) + 3000);
+
+// tiempo
+const tiempoCaida = Math.round(Math.random() * (60 - 45) + 45);
+
+// Reto extra
+const velocidadPromedio = (altura / tiempoCaida).toFixed(2);
+
+// resultado interpolacion
+console.log("--- Reporte de Salto en Paracaídas ---");
+console.log(`Fecha del salto: ${fechaFormateada}`);
+console.log(`Altitud de salto: ${altura} metros`);
+console.log(`Duración en caída libre: ${tiempoCaida} segundos`);
+console.log(`Velocidad promedio: ${velocidadPromedio} m/s`);
+
+// validacion de reto 
+if (altura > 4000) {
+    console.log("Estado: Salto de alta altitud (Requiere oxígeno).");
+} else {
+    console.log("Estado: Salto estándar.");
+}
