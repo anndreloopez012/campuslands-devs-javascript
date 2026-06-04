cf9f218514acec5630eaac1416b65b48216d5a29
danielAVAR <danielaguilar5502@gmail.com>
2026-06-03 22:58:03 -0600

    feat: resolvi ejercicio 6

--- ejercicios/06-objetos-viajes/ejercicio.js
+++ ejercicios/06-objetos-viajes/ejercicio.js
@@ -5,3 +5,18 @@ console.log("Ejercicio 06: Objetos");
 
 // Escribe tu solucion aqui.
 
+const viaje = {
+    destino: "Paris",
+    duracion: 7,
+    presupuesto: 1500,
+    actividades: ["visitar museos", "pasear por la ciudad", "probar comida local"],
+    motivo : function() {
+        return "voy de vacaciones a " + this.destino;
+    }
+};
+
+console.log(viaje);
+
+console.log(viaje.destino);
+
+console.log(viaje.motivo());
