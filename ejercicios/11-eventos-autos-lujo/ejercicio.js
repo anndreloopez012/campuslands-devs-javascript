42e64736448b94e67bb57976452953aab53db1cb
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 21:24:02 -0600

    :sparkles: Feat(ejercicio-12): Ejercicio fechas y math - paracaidismo

--- ejercicios/11-eventos-autos-lujo/ejercicio.js
+++ ejercicios/11-eventos-autos-lujo/ejercicio.js
@@ -8,14 +8,16 @@ console.log("Ejercicio 11: Eventos");
 let main = document.querySelector(".panel")
 let titulo = document.querySelector("#titulo")
 let descripcion = document.querySelector("#descripcion")
+let input = document.querySelector("#input")
 let boton = document.querySelector("#accion")
 
 
 boton.addEventListener("click", function() {
-    main.style.backgroundColor = "black"
+    main.style.backgroundColor = "darkblue"
     titulo.style.color = "white"
     descripcion.style.color = "white"
-    boton.style.backgroundColor = "green" 
-    titulo.textContent = "Lamborghini Aventador"
+    boton.style.backgroundColor = "darkgreen" 
+    titulo.textContent = input.value || "Lamborghini Aventador"
     descripcion.textContent = "El Lamborghini Aventador es un superdeportivo de lujo fabricado por la marca italiana Lamborghini. Con su diseño agresivo y su potente motor V12, el Aventador ofrece un rendimiento excepcional y una experiencia de conducción emocionante. Es conocido por su velocidad, manejo preciso y exclusividad, convirtiéndolo en uno de los autos más deseados del mundo."
+    main.removeChild(input)
 })
--- ejercicios/12-fechas-math-paracaidismo/ejercicio.js
+++ ejercicios/12-fechas-math-paracaidismo/ejercicio.js
@@ -5,3 +5,12 @@ console.log("Ejercicio 12: Fechas y Math");
 
 // Escribe tu solucion aqui.
 
+console.log("========== Fechas y Math - paracaidismo ==========");
+
+const altura = Math.floor(Math.random() * (4000 - 800 + 1)) + 800
+const tiempo = +(Math.random() * (300 - 30) + 30).toFixed(1)
+const fecha = new Date()
+
+console.log(`Altura: ${altura} metros`)
+console.log(`Tiempo de caída: ${tiempo} segundos`)
+console.log(`Fecha: ${fecha.toLocaleDateString()}`)
