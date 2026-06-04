46a1273bb1e3fcb41ceee599a8ce4350fef2c439
lemical07 <icaleladio@gmail.com>
2026-06-03 20:58:26 -0600

    feat: crear objeto viaje con metodos internos, uso de de 'this' y calculo de presupuesto diario

--- ejercicios/06-objetos-viajes/ejercicio.js
+++ ejercicios/06-objetos-viajes/ejercicio.js
@@ -5,3 +5,27 @@ console.log("Ejercicio 06: Objetos");
 
 // Escribe tu solucion aqui.
 
+// Creación del objeto viaje con sus propiedades y métodos
+const viajeDestino = {
+  destino: "Taiwán",
+  dias: 15,
+  presupuestoTotal: 25000,
+
+  // extra: presupuesto por dia
+  calcularPresupuestoDiario: function() {
+    return this.presupuestoTotal / this.dias;
+  },
+
+  mostrarResumen: function() {
+    console.log("=== RESUMEN DEL VIAJE ===");
+    console.log("Destino seleccionado:", this.destino);
+    console.log("Duración de la estadía:", this.dias, "días");
+    console.log("Presupuesto total asignado:", this.presupuestoTotal);
+
+    const diario = this.calcularPresupuestoDiario();
+    console.log("Presupuesto disponible por día:", diario.toFixed(2));
+  }
+};
+
+// Ejecución del método del objeto
+viajeDestino.mostrarResumen();
