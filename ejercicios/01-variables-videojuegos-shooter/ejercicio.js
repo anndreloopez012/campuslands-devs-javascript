9a5d9e9befa650273b8a83ccf170f2c7dbe46a6a
steff-360 <steff.18random@gmail.com>
2026-06-04 06:24:04 -0600

    Resolución del ejercicio 01

--- ejercicios/01-variables-videojuegos-shooter/ejercicio.js
+++ ejercicios/01-variables-videojuegos-shooter/ejercicio.js
@@ -5,3 +5,34 @@ console.log("Ejercicio 01: Variables y constantes");
 
 // Escribe tu solucion aqui.
 
+console.log("Ejercicio 01: Variables y constantes");
+
+// Variables
+let jugador = "Ghost";
+let arma = "AK-47";
+let vida = 100;
+let municion = 30;
+let partidaActiva = true;
+
+// Constante
+const mapa = "Desert Storm";
+
+// Mostrar información
+console.log("Jugador:", jugador);
+console.log("Arma:", arma);
+console.log("Vida:", vida);
+console.log("Munición:", municion);
+console.log("Partida activa:", partidaActiva);
+console.log("Mapa:", mapa);
+
+// Reasignación de valores
+vida = 80;
+municion = 20;
+
+console.log("Después del combate:");
+console.log("Vida restante:", vida);
+console.log("Munición restante:", municion);
+
+// Reto extra
+let enemigosEliminados = 5;
+console.log("Enemigos eliminados:", enemigosEliminados);
