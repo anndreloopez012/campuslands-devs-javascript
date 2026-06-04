76fcb2e2261c81430a067fa041d6e271d6c34beb
danielAVAR <danielaguilar5502@gmail.com>
2026-06-03 21:14:49 -0600

    feat: resolvi ejercicio 2

--- ejercicios/02-tipos-datos-motos/ejercicio.js
+++ ejercicios/02-tipos-datos-motos/ejercicio.js
@@ -4,4 +4,24 @@
 console.log("Ejercicio 02: Tipos de datos");
 
 // Escribe tu solucion aqui.
+//Atributos de moto
 
+let marca = "Yamaha";
+let kilometraje = 15000;
+let tipo = "electrica";
+let disponibilidad = true;
+let colores_disponibles = ["azul", "plateado", "negro"];
+let modelo = undefined;
+let dueño = null;
+let cantidad_de_motos = 5n;
+let id = Symbol("moto67");
+
+console.log(typeof marca);
+console.log(typeof kilometraje);
+console.log(typeof tipo);
+console.log(typeof disponibilidad);
+console.log(typeof colores_disponibles);
+console.log(typeof modelo);
+console.log(typeof dueño);
+console.log(typeof cantidad_de_motos);
+console.log(typeof id);
