1517925d8f78892703aa308024a804ef8af2d752
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-03 21:43:50 -0600

    :sparkles: Feat(ejercicio-13):Ejercicio-13-json-libros-peliculas

--- ejercicios/13-json-libros-peliculas/ejercicio.js
+++ ejercicios/13-json-libros-peliculas/ejercicio.js
@@ -5,3 +5,19 @@ console.log("Ejercicio 13: JSON");
 
 // Escribe tu solucion aqui.
 
+console.log("========== JSON - Libros y Peliculas ==========");
+
+const libros = ["La vuelta al mundo en 80 dias", "El principe y el mendigo", "El conde de montecristo", "Dueño del mundo", "El hombre invisible "];
+const peliculas = ["El señor de los anillos", "El hobbit", "El rey leon", "El libro de la selva", "El diario de una pasion"];
+
+const librosJSON = JSON.stringify(libros);
+const peliculasJSON = JSON.stringify(peliculas);
+
+console.log("Libros en formato JSON:", librosJSON);
+console.log("Peliculas en formato JSON:", peliculasJSON);
+
+const librosArray = JSON.parse(librosJSON);
+const peliculasArray = JSON.parse(peliculasJSON);
+
+console.log("Libros convertidos de JSON a array:", librosArray);
+console.log("Peliculas convertidos de JSON a array:", peliculasArray);
