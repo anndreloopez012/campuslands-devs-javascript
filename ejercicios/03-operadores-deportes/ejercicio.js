06fc51452a4b6867fd722478c83ac94a161e3009
steff-360 <steff.18random@gmail.com>
2026-06-04 06:45:57 -0600

    Resolucion ejercicio 04

--- ejercicios/03-operadores-deportes/ejercicio.js
+++ ejercicios/03-operadores-deportes/ejercicio.js
@@ -1,9 +1,4 @@
 // Ejercicio 03: Operadores matematicos - deportes
-// Completa el codigo siguiendo las instrucciones del README.md.
-
-console.log("Ejercicio 03: Operadores matematicos");
-
-// Escribe tu solucion aqui.
 
 console.log("Ejercicio 03: Operadores matematicos");
 
--- ejercicios/04-condicionales-videojuegos-futbol/ejercicio.js
+++ ejercicios/04-condicionales-videojuegos-futbol/ejercicio.js
@@ -1,7 +1,25 @@
 // Ejercicio 04: Condicionales if/else - videojuegos de futbol
-// Completa el codigo siguiendo las instrucciones del README.md.
 
 console.log("Ejercicio 04: Condicionales if/else");
 
-// Escribe tu solucion aqui.
+// Goles del jugador y del rival
+let golesJugador = 3;
+let golesRival = 2;
 
+// Comparar resultados
+if (golesJugador > golesRival) {
+    console.log("¡Victoria! Ganaste el partido.");
+} else if (golesJugador === golesRival) {
+    console.log("Empate. El partido terminó igualado.");
+} else {
+    console.log("Derrota. El rival ganó el partido.");
+}
+
+// Reto extra
+let tirosAlArco = 8;
+
+if (tirosAlArco >= 5) {
+    console.log("Buen rendimiento ofensivo.");
+} else {
+    console.log("Debes mejorar los ataques al arco.");
+}
