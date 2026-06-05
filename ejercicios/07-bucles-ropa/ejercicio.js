8294e5e4ef496c3ec604b06db350fb77c8aef24e
danielAVAR <danielaguilar5502@gmail.com>
2026-06-04 19:32:50 -0600

    feat: resolvi ejercicio 7

--- ejercicios/07-bucles-ropa/ejercicio.js
+++ ejercicios/07-bucles-ropa/ejercicio.js
@@ -5,3 +5,20 @@ console.log("Ejercicio 07: Bucles for/while");
 
 // Escribe tu solucion aqui.
 
+// Lista de ropa
+
+let ropa = ["camisa", "pantalon", "zapatos", "sombrero", "chaqueta"];
+
+// Bucle for para imprimir cada prenda de ropa
+
+for (let i = 0; i < ropa.length; i++) {
+    console.log(ropa[i]);
+}
+
+// Bucle while para imprimir cada prenda de ropa
+
+let j = 0;
+while (j < ropa.length) {
+    console.log(ropa[j]);
+    j++;
+}
