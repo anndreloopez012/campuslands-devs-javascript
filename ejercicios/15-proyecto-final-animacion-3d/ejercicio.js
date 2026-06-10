279d5bd6dc4204300648e61a2696e6000b891d82
ArrivillagaDAS <sfaaldana101@gmail.com>
2026-06-10 13:07:46 -0600

    fix: reubicacion del archivo - ejercicio 15

--- ejercicios/15-proyecto-final-animacion-3d/ejercicio.js
+++ ejercicios/15-proyecto-final-animacion-3d/ejercicio.js
@@ -4,30 +4,3 @@
 console.log("Ejercicio 15: Proyecto integrador basico");
 
 // Escribe tu solucion aqui.
-
-// Seleccionamos elementos
-const btn = document.querySelector("#toggle");
-const cubo = document.querySelector(".cube");
-const torre = document.querySelector(".tower");
-
-// Estado de la animacion (Reto extra: validacion de estado)
-let estaAnimado = false;
-
-btn.addEventListener("click", () => {
-    if (!estaAnimado) {
-        // animacion
-        cubo.style.transform = "rotateX(20deg) rotateY(35deg) translateY(-50px)";
-        torre.style.transform = "scaleY(1.2)";
-        btn.textContent = "Resetear";
-        console.log("Escena: Modo animado activado");
-    } else {
-        // reseteo
-        cubo.style.transform = "rotateX(20deg) rotateY(35deg) translateY(0px)";
-        torre.style.transform = "scaleY(1)";
-        btn.textContent = "Animar";
-        console.log("Escena: Estado inicial");
-    }
-    
-    // Cambiamos el valor de la validacion
-    estaAnimado = !estaAnimado;
-});
