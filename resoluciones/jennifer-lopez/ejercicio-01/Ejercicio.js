a5cdabcdfc0f4ff72ec05381308a85d019eaeff2
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-09 21:04:00 -0600

    feat: se realizo solucion para el ejercicio 01

---
+++ resoluciones/jennifer-lopez/ejercicio-01/Ejercicio.js
@@ -0,0 +1,29 @@
+// Ejercicio 01: Variables y constantes - videojuegos shooter
+console.log("Ejercicio 01: Variables y constantes");
+
+//-----Declaracion de variables----
+//Datos que no cambiaran
+const jugador="Goldenboy";
+const arma="AK-47";
+//Datos que cambiaran durante el juego
+let vida =100;
+let municion=30;
+let estado= "En combate";
+console.log("\n--INICIO DE LA PARTIDA--\n");
+console.log("Bienvenido " , jugador);
+console.log("Tu arma es: " , arma);
+console.log("Tu vida actual es: " , vida);
+console.log("Tu municion es: " , municion, " balas");
+console.log("El estado de tu partida es: " , estado);
+
+ console.log("\nEntrando en combate..\n");
+ municion=municion-25;
+ vida=vida-100;
+ console.log("Disparaste tu arma, te quedan " , municion , " balas");
+ console.log("Has sido atacado, tu vida actual es: " , vida);
+ estado="En peligro";
+console.log("El estado de tu partida es: " , estado);
+//estado del jugador
+let jugadorEliminado = vida <= 0;
+console.log("¿Jugador eliminado?: " + jugadorEliminado);
+console.log("\n--COMBATE FINALIZADO--\n");
