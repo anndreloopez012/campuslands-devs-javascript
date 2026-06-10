8e5475356ba3f27724a517dc543a5438e9b55485
MariaJoseMontepequeZet <montepequemajo@gmail.com>
2026-06-10 12:56:09 -0600

    Resolver: ejercicio 4 javascript

---
+++ resoluciones/maria-montepeque/maria-montepeque.js
@@ -0,0 +1,23 @@
+// Ejercicio 04: Condicionales if/else - videojuegos de futbol
+
+console.log("Ejercicio 04: Condicionales if/else");
+
+// Escribe tu solucion aqui.
+
+const local = "FC Barcelona",
+  visitante = "Real Madrid";
+const golesLocal = 3,
+  golesVisitante = 1;
+
+console.log(`${local} ${golesLocal} - ${golesVisitante} ${visitante}`);
+
+if (golesLocal > golesVisitante) {
+  console.log(`Victoria de ${local}`);
+} else if (golesLocal < golesVisitante) {
+  console.log(`Derrota de ${local}, gana ${visitante}`);
+} else {
+  console.log("Empate");
+}
+
+const diferencia = Math.abs(golesLocal - golesVisitante);
+if (diferencia >= 3) console.log(`Goleada por ${diferencia} goles!`);
