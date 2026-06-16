113f0a0fec3941c551470565e30bda5ffb8d49bf
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-15 19:54:06 -0600

    :rocket: Ejercicios: Ejercicios de JavaScript

---
+++ ejercicios/resoluciones/anderson-oloroso/01-variables-videojuegos-shooter/ejercicio.js
@@ -0,0 +1,20 @@
+// Ejercicio 01: Variables y constantes - videojuegos shooter
+// Completa el codigo siguiendo las instrucciones del README.md.
+
+console.log("Ejercicio 01: Variables y constantes");
+
+// Escribe tu solucion aqui.
+
+const jugador = 'Alondra'
+let vida = '150 HP'
+
+const arma = 'Locus'
+let municion = '30'
+
+const estadoPartida = ['Emparejamiento', 'Pre-partida', 'Partida en curso', 'Post-partida']
+
+console.log(jugador)
+console.log(vida)
+console.log(arma)
+console.log(municion)
+console.log(estadoPartida)
