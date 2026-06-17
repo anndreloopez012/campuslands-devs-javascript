426982ea04ae25546e4944618790534f3e7eff0b
Jose-langL <totherrerajoseluis@gmail.com>
2026-06-17 07:24:31 -0600

    merge: Alumno/joseluis herrera/ejer08
    * feat(ejercicio08) Resolver ejercicio de metodos string
    
    * fix resolucion del ejercicio 08
    
    * fix: Corregi errores de ejercicio-08

---
+++ resoluciones/joseluis-herrera/ejercicio-08/joseluis-herrera.js
@@ -0,0 +1,16 @@
+let cancionPuerka = " Amor malandro - Mercado Negro ";
+
+//Metodo - trim
+console.log(cancionPuerka.trim())
+
+//Metodo toUpperCase
+console.log(cancionPuerka.toUpperCase())
+
+//Metodo incluides
+console.log(cancionPuerka.includes("Amor"))
+
+//Metodo replace
+console.log(cancionPuerka.replace("Amor", "maliBu"))
+
+//Metodo slice
+console.log(cancionPuerka.slice(1,5))
