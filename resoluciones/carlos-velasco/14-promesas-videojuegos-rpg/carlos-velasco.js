c73c0691d7de55b46a9fb19e73165af8d2d3e198
Carlos Elias Tzoy <carlos.velasco.est@gmail.com>
2026-06-18 07:29:53 -0600

    merge: feat(resoluciones): Ejercicio 14 finalizado

---
+++ resoluciones/carlos-velasco/14-promesas-videojuegos-rpg/carlos-velasco.js
@@ -0,0 +1,38 @@
+const abrircofre = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const llave = true
+            if (llave) {
+                resolve("¡¡ abriendo cofre !!")
+            } else {
+                reject("¡¡ sin la llave del cofre !!")
+            }
+        }, 2000)
+    })
+}
+
+const obtenerRecurso = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const abierto = true
+            if (abierto) {
+                resolve("¡¡ obtuvistes 50 monedas de oro !!")
+            } else {
+                reject("¡¡ sin la llave del cofre !!")
+            }
+        }, 3000)
+    })
+}
+
+const ejecutar = async () => {
+    try {
+        console.log("PJ1 intenta abrir cofre")
+        const abrir = await abrircofre()
+        console.log(abrir)
+        const recurso = await obtenerRecurso()
+        console.log(recurso)
+    } catch (error) {
+        console.error(error)
+    }
+}
+ejecutar();
