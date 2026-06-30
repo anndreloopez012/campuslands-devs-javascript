e81e2d89df1ff977483d380fa9fe42d9b1fbdb29
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-06-30 16:35:14 -0600

    feat: :sparkles: feat(resoluciones: ejercicio 01 javascript finalizado
    ejercicio 01 de javascript finalizado
    
    hecho por Evelyn

---
+++ resoluciones/evelyn-barrios/01-variables-videojuegos-shooter/evelyn-barrios.js
@@ -0,0 +1,20 @@
+// Ejercicio 01: Variables y constantes - videojuegos shooter
+// Completa el codigo siguiendo las instrucciones del README.md.
+
+console.log("Ejercicio 01: Variables y constantes");
+
+// Escribe tu solucion aqui.
+const nombreDelMapa = "Estación Orbital";
+const aliasDelJugador = "Evelyn 'Nova'";
+
+
+let vidaActual = 100;
+let balasEnReserva = 150;
+let puntosAcumulados = 0;
+
+// --- Mostrando la información inicial en consola ---
+console.log("--- INICIO DE MISIÓN ---");
+console.log("Jugador: " + aliasDelJugador);
+console.log("Mapa: " + nombreDelMapa);
+console.log("Vida: " + vidaActual + "%");
+console.log("Balas de reserva: " + balasEnReserva);
