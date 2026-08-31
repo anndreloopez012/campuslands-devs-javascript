4bb61336715dafd47b7ddab69eed6b9d0e0b7296
Daniel <danielaguilar5502@gmail.com>
2026-08-31 06:31:57 -0600

    feat: ejercicio 001 (#264)

---
+++ resoluciones/daniel-aguilar/ejercicio-001.js
@@ -0,0 +1,26 @@
+// Ejercicio 01: Variables y constantes - videojuegos shooter
+// Completa el codigo siguiendo las instrucciones del README.md.
+
+console.log("Ejercicio 01: Variables y constantes");
+
+// Escribe tu solucion aqui.
+
+let usuario = "carlos_Uchiha_sasuke";
+const health_points = 67;
+let ammo = 5000;
+let arma = "bazooka";
+let online = true;
+let skin = "skin de profe andre";
+skin = "skin de prode edwin";
+
+console.log("Usuario: " + usuario);
+console.log("Health Points: " + health_points);
+console.log("Ammo: " + ammo);
+console.log("Arma: " + arma);
+console.log("Online: " + online);
+console.log("Skin: " + skin);
+
+// popo
+
+
+
